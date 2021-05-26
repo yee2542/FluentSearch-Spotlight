939a1e7c0ad73f0357b0bc02092ea96848599f33
yee2542 <yee2542gtl@gmail.com>
2021-05-27 03:49:54 +0700

    register els module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
+import { ElasticsearchModule } from '@nestjs/elasticsearch';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from './config/config.module';
+import { ConfigService } from './config/config.service';
 
 @Module({
-  imports: [ConfigModule],
+  imports: [
+    ConfigModule,
+    ElasticsearchModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        node: configService.get().elastic.node,
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
