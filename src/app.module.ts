a495a44cad09909d2130626cb721fd745a39808f
yee2542 <yee2542gtl@gmail.com>
2021-05-27 03:59:33 +0700

    remove els

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,10 @@
 import { Module } from '@nestjs/common';
-import { ElasticsearchModule } from '@nestjs/elasticsearch';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from './config/config.module';
-import { ConfigService } from './config/config.service';
 
 @Module({
-  imports: [
-    ConfigModule,
-    ElasticsearchModule.registerAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        node: configService.get().elastic.node,
-      }),
-    }),
-  ],
+  imports: [ConfigModule],
   controllers: [AppController],
   providers: [AppService],
 })
