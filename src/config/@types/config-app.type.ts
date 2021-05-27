d508839c7b9c8e2ea37c72cbff271c522530e35f
yee2542 <korbboon.thuswongsa@mail.kmutt.ac.th>
2021-05-27 09:59:13 +0700

    Init (#1)
    * init
    
    * update tsconfig
    
    * fix bad interlop module
    
    * add setting ws
    
    * add docker image
    
    * add ci
    
    * add mock gql
    
    * add docker-compose
    
    * fix bad compose file
    
    * ignore db
    
    * remove minio
    
    * add els
    
    * add config serivce
    
    * remove unused config
    
    * add els deps
    
    * refactor
    
    * add els config
    
    * add config env
    
    * add mongoose
    
    * register els module
    
    * fix bad els endpoint
    
    * remove els
    
    * add mq
    
    * add mq lib
    
    * add fls types
    
    * fix bad ci
    
    * add gql
    
    * remove unused config
    
    * remove unused deps

---
+++ src/config/@types/config-app.type.ts
@@ -0,0 +1,18 @@
+export type ConfigAppProviderType = {
+  hostname: string;
+  main_hostname: string;
+  database: {
+    connection: string;
+    username: string;
+    password: string;
+    authSource: string;
+  };
+  jwt: {
+    secretKey: string;
+    expires: number;
+  };
+  opsKey: string;
+  node_env: 'production' | 'development';
+  origin: RegExp;
+  port: number;
+};
---
+++ src/config/config.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigAppProviderType } from './@types/config-app.type';
+import { ConfigEnvType } from './@types/config-env.type';
+
+@Injectable()
+export class ConfigService {
+  get(): ConfigAppProviderType {
+    const {
+      DATABASE_CONNECTION,
+      DATABASE_USERNAME,
+      DATABASE_PASSWORD,
+      DATABASE_AUTH_SOURCE,
+      JWT_SECRET_KEY,
+      JWT_EXPIRES,
+      OPS_KEY,
+      ORIGIN,
+      PORT,
+      HOSTNAME,
+      MAIN_HOSTNAME,
+    } = process.env as ConfigEnvType;
+    return {
+      hostname: HOSTNAME,
+      main_hostname: MAIN_HOSTNAME,
+      database: {
+        connection:
+          DATABASE_CONNECTION ||
+          'mongodb://mongodb-sharded:27017/fluentsearch-bff',
+        username: DATABASE_USERNAME,
+        password: DATABASE_PASSWORD,
+        authSource: DATABASE_AUTH_SOURCE,
+      },
+      jwt: {
+        secretKey: JWT_SECRET_KEY || 'FluentSearch.JWT.SECRET',
+        expires: Number(JWT_EXPIRES || 300000), // 5 minutes
+      },
+      opsKey: OPS_KEY || 'FluentSearch.BFF.OpsKey',
+      node_env:
+        (process.env.NODE_ENV as ConfigAppProviderType['node_env']) ||
+        'development',
+      origin: new RegExp(ORIGIN),
+
+      port: Number(PORT || 3000),
+    };
+  }
+}
