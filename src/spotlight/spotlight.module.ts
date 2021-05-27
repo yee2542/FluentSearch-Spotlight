35b9834d5c5b7b6a430c29c503147857877244e0
yee2542 <yee2542gtl@gmail.com>
2021-05-27 10:05:24 +0700

    register insight schema

--- src/spotlight/spotlight.module.ts
+++ src/spotlight/spotlight.module.ts
@@ -1,7 +1,15 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { SpotlightService } from './spotlight.service';
+import { INSIGHT_SCHEMA_NAME } from 'fluentsearch-types';
+import insightSchema from 'fluentsearch-types/dist/entity/insight.entity';
 
 @Module({
-  providers: [SpotlightService]
+  imports: [
+    MongooseModule.forFeature([
+      { name: INSIGHT_SCHEMA_NAME, schema: insightSchema },
+    ]),
+  ],
+  providers: [SpotlightService],
 })
 export class SpotlightModule {}
