a0b579fa3e315910726e8b7de6eb35036a71d7e9
yee2542 <yee2542gtl@gmail.com>
2021-05-31 07:00:53 +0700

    add model

--- src/spotlight/dto/file-video-insight.dto.ts
+++ src/spotlight/dto/file-video-insight.dto.ts
@@ -1,6 +1,20 @@
 import { Field, ObjectType } from '@nestjs/graphql';
+import { ModelEnum } from 'fluentsearch-types';
 import { FileInsightMeta } from './file-meta.dto';
 import { InsightVideoDTO } from './insight-video.dto';
+
+@ObjectType()
+export class Dimension {
+  @Field(() => Number)
+  original_width: number;
+  @Field(() => Number)
+  original_height: number;
+
+  @Field(() => Number)
+  extract_width: number;
+  @Field(() => Number)
+  extract_height: number;
+}
 
 @ObjectType()
 export class FileVideoInsightDto {
@@ -9,4 +23,10 @@ export class FileVideoInsightDto {
 
   @Field(() => [InsightVideoDTO])
   insights: InsightVideoDTO[];
+
+  @Field(() => Dimension)
+  dimension: Dimension;
+
+  @Field(() => ModelEnum)
+  model: ModelEnum;
 }
